b649a6fb501ddfdc19bdff25ad12002ec16c9af0
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-10 03:42:01 +0300

    create a method to add a tree to the database.

--- app/src/main/java/com/example/newsapi/dao/daos.kt
+++ app/src/main/java/com/example/newsapi/dao/daos.kt
@@ -3,6 +3,7 @@ package com.example.newsapi.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import com.example.newsapi.models.Tree
 import com.example.newsapi.models.User
 import com.example.newsapi.models.Vendor
 
@@ -15,10 +16,17 @@ interface UserDao{
 
 }
 
+@Dao
 interface VendorDao{
     @Insert
     fun addVendor(vendor: Vendor)
     @Query("select * from vendors")
     fun getVendors(): List<Vendor>
 
+}
+
+@Dao
+interface  TreeDao{
+    @Insert
+    fun addTree(tree: Tree)
 }
