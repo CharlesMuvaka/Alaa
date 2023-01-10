7f3ec6d8ccfa7e5fc2b81817398a0c624daae5d1
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-10 16:12:08 +0300

    bind the recycler view with the adapter.

---
+++ app/src/main/java/com/example/newsapi/fragments/ShopFragment.kt
@@ -0,0 +1,60 @@
+package com.example.newsapi.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.room.Room
+import com.example.newsapi.DatabaseClient
+import com.example.newsapi.R
+import com.example.newsapi.adapters.TreeRecyclerAdapter
+import com.example.newsapi.databinding.FragmentShopBinding
+import com.example.newsapi.models.Tree
+
+class ShopFragment : Fragment() {
+
+    private lateinit var bind: FragmentShopBinding
+    private lateinit var seedlings:List<Tree>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        bind = FragmentShopBinding.inflate(layoutInflater)
+
+        val databaseClient = context?.let {
+            Room.databaseBuilder(
+                it.applicationContext,
+                DatabaseClient::class.java, "tek_database").fallbackToDestructiveMigration().allowMainThreadQueries().build()
+        }
+        if (databaseClient != null){
+            seedlings = databaseClient.treeDao.getTrees()
+        }
+        bind.myRecView.apply {
+            adapter = context?.let { TreeRecyclerAdapter(seedlings, it) }
+            layoutManager = GridLayoutManager(context, 1)
+            hasFixedSize()
+        }
+        return bind.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ShopFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
