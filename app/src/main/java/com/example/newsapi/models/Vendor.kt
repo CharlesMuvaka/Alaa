f6cda2b5ac424c67aecd5ae329ac127b72b97ee0
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-10 14:06:17 +0300

    add member variables and mapped them to the database.

--- app/src/main/java/com/example/newsapi/models/Vendor.kt
+++ app/src/main/java/com/example/newsapi/models/Vendor.kt
@@ -1,7 +1,15 @@
 package com.example.newsapi.models
 
-class Vendor(private val name:String, private val phone:Int, private val location:String) {
-    private val id:Int? = null
-    private var website: String? = null
-    private var trees: List<Tree>? = null
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "vendors")
+data class Vendor(@ColumnInfo(name = "vendor_name")val name:String, @ColumnInfo(name = "vendor_phone")val phone:String,
+             @ColumnInfo(name = "vendor_email")val email:String, @ColumnInfo(name = "vendor_location")val location:String):java.io.Serializable {
+    @ColumnInfo(name = "vendor_id")
+    @PrimaryKey(autoGenerate = true)
+    var id:Int? = null
+    @ColumnInfo(name = "vendor_website")
+    var website: String? = null
 }
