86db8c3d0e4435a861ae1aa6371467c247953352
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-10 14:06:30 +0300

    add member variables and mapped them to the database.

--- app/src/main/java/com/example/newsapi/models/User.kt
+++ app/src/main/java/com/example/newsapi/models/User.kt
@@ -1,6 +1,12 @@
 package com.example.newsapi.models
 
-class User(private val name:String, private val phone:Int, private val email:String) {
-    private var id:Int? = null
-    private var location: String? = null
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+class User(@ColumnInfo(name = "user_name") val name:String, val phone:Int, val email:String) :java.io.Serializable{
+    @PrimaryKey(autoGenerate = true)
+    var id:Int? = null
+    var location: String? = null
 }
