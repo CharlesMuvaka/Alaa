74d837e16584421072f874f8fb58c0da875f544e
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-10 04:59:43 +0300

    add member variables and mapped them in the database.

--- app/src/main/java/com/example/newsapi/models/Tree.kt
+++ app/src/main/java/com/example/newsapi/models/Tree.kt
@@ -1,4 +1,17 @@
 package com.example.newsapi.models
 
-class Tree {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "trees")
+data class Tree( @ColumnInfo(name = "tree_name")val name:String, @ColumnInfo(name = "tree_species")val species:String) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+    @ColumnInfo(name = "owner_id")
+    var ownerId: Int = 0
+    @ColumnInfo(name = "tree_description")
+    var description: String? = null
+    @ColumnInfo(name = "tree_image_url")
+    var image: Int? = null
 }
