7006a9f4dd5add82123fbc9675e438b6b4897c6e
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-11 05:03:26 +0300

    define a method that intercepts the api request and adds the auth token in the header.

---
+++ app/src/main/java/com/example/newsapi/interceptors/AuthInterceptor.kt
@@ -0,0 +1,15 @@
+package com.example.newsapi.interceptors
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+class AuthInterceptor(private val authToken:String): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request:Request = chain.request()
+            .newBuilder()
+            .addHeader("Authorization", "Bearer $authToken")
+            .build()
+        return chain.proceed(request)
+    }
+}
