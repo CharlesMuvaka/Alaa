3b06d420f3833971db9bb7b4c51a3ae9e97d8167
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-11 05:01:23 +0300

    add a method that validates the user phone number based on the apis requirements.

--- app/src/main/java/com/example/newsapi/utils/Validator.kt
+++ app/src/main/java/com/example/newsapi/utils/Validator.kt
@@ -7,5 +7,18 @@ class Validator {
     fun getTime():String{
         return SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(Date())
     }
+    fun checkPhoneNumber(phoneNumber:String):String{
+        if (phoneNumber == ""){
+            return ""
+        }
+        if (phoneNumber.length < 11 && phoneNumber.startsWith("0")) {
+            return phoneNumber.replace("^0", "254")
+        }
+        if (phoneNumber.length == 13 && phoneNumber.startsWith("+")){
+            return phoneNumber.replace("^+", "")
+        }
+        return phoneNumber
+    }
+
 
 }
