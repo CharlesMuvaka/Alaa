977816303b94be845ccf7fb0710771c52f5a4f2c
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-13 02:19:44 +0300

    define constants to be used throughout the application.

---
+++ app/src/main/java/com/example/newsapi/models/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.newsapi.models
+
+
+const val BASEURL = "https://sandbox.safaricom.co.ke/"
+const val CONNECT_TIMEOUT = 60 * 1000
+const val READ_TIMEOUT = 60 * 1000
+const val WRITE_TIMEOUT = 60 * 1000
+const val BUSINESS_SHORT_CODE ="247247"
+const val PASS_KEY = "Ejve1mAALX5ShahOX5T1XAdK8u6sOp4h"
+const val TRANSACTION_TYPE = "CustomerPayBillOnline"
+const val PARTY_B = "1320180810489"
+const val CALL_BACK_URL = "https://mpesa-requestbin.herokuapp.com/14vfhf01"
