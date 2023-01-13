143b6799bb9e7ff62bde918a1cba96d37aa125dc
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-13 05:03:13 +0300

    fix: try to access views from the host activity using the nav controller.

--- app/src/main/java/com/example/newsapi/fragments/MainFragment.kt
+++ app/src/main/java/com/example/newsapi/fragments/MainFragment.kt
@@ -6,14 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.get
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapi.R
 import com.example.newsapi.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(), View.OnClickListener {
     private lateinit var  bind: FragmentMainBinding
+    private lateinit var navHostFragment: NavHostFragment
 
 
 
@@ -30,10 +33,18 @@ class MainFragment : Fragment(), View.OnClickListener {
         // Inflate the layout for this fragment
         bind = FragmentMainBinding.inflate(layoutInflater)
 
-        val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
 
         bind.image3.setOnClickListener(this::onClick)
         bind.navView.bringToFront()
+
+        var fragment = navHostFragment.navController.currentDestination!!.label
+        var graph = navHostFragment.navController.graph[R.id.treeFragment]
+//        bind.slogan.text = "$fragment ${navHostFragment.id} "
+//        bind.vendors.text = graph.id.toString()
+
+
+
         bind.navView.setupWithNavController(navHostFragment.navController)
         return bind.root
     }
@@ -52,4 +63,5 @@ class MainFragment : Fragment(), View.OnClickListener {
             bind.drawer.openDrawer(GravityCompat.START)
         }
     }
+
 }
